6a15bba9af3922bd55d95e08af8661ae26ee747a
Walfie <walfington@gmail.com>
2020-05-12 23:19:50 -0400

    Add subscription

--- src/raid_handler.rs
+++ src/raid_handler.rs
@@ -1,21 +1,68 @@
 use std::ops::Deref;
+use std::pin::Pin;
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 use crate::model::{Boss, BossName, CachedString, Language, Raid};
 
 use circular_queue::CircularQueue;
 use dashmap::DashMap;
+use futures_util::stream::Stream;
 use parking_lot::RwLock;
 use tokio::sync::broadcast;
 
 #[derive(Clone, Debug, PartialEq, Eq, Hash)]
 struct BossKey(BossName);
 
+#[derive(Clone, Debug)]
 pub struct RaidHandler(Arc<RaidHandlerInner>);
+
+pin_project_lite::pin_project! {
+    pub struct Subscription {
+        #[pin]
+        rx: broadcast::Receiver<Arc<Raid>>,
+        boss_name: BossName,
+        handler: Arc<RaidHandlerInner>,
+    }
+}
+
+impl Stream for Subscription {
+    type Item = Arc<Raid>;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<Self::Item>> {
+        let mut this = self.project();
+
+        loop {
+            match futures_util::ready!(this.rx.as_mut().poll_next(cx)) {
+                Some(Ok(item)) => return Poll::Ready(Some(item)),
+                Some(Err(broadcast::RecvError::Lagged(_))) => continue,
+                Some(Err(broadcast::RecvError::Closed)) => (),
+                None => (),
+            }
+
+            // Attempt to resubscribe if the inner subscription ends
+            // (e.g., if the stream is stopped due to being merged with another boss)
+            std::mem::replace(
+                &mut *this.rx,
+                this.handler.subscribe(this.boss_name.clone()),
+            );
+        }
+    }
+}
 
 impl RaidHandler {
     pub fn new(capacity: usize) -> Self {
         Self(Arc::new(RaidHandlerInner::new(capacity)))
+    }
+
+    pub fn subscribe(&self, boss_name: BossName) -> Subscription {
+        let inner = self.0.clone();
+
+        Subscription {
+            rx: inner.subscribe(boss_name.clone()),
+            boss_name,
+            handler: inner.clone(),
+        }
     }
 }
 
@@ -27,6 +74,7 @@ impl Deref for RaidHandler {
     }
 }
 
+#[derive(Debug)]
 pub struct RaidHandlerInner {
     bosses: DashMap<BossKey, Arc<Boss>>,
     raid_history: DashMap<BossKey, RwLock<CircularQueue<Arc<Raid>>>>,
@@ -48,6 +96,18 @@ impl RaidHandlerInner {
             translations: DashMap::new(),
             boss_broadcast: tx,
             capacity,
+        }
+    }
+
+    fn subscribe(&self, boss_name: BossName) -> broadcast::Receiver<Arc<Raid>> {
+        let key = self.boss_key(boss_name);
+
+        if let Some(guard) = self.raid_broadcast.get(&key) {
+            guard.value().subscribe()
+        } else {
+            let (tx, rx) = broadcast::channel(self.capacity);
+            self.raid_broadcast.insert(key, tx);
+            rx
         }
     }
 
@@ -116,6 +176,7 @@ impl RaidHandlerInner {
             }
 
             self.translations.insert(src, dst);
+            self.raid_broadcast.remove(&src_key);
 
             let _ = self.boss_broadcast.send(boss);
         }
@@ -176,14 +237,19 @@ mod test {
     use crate::model::LangString;
     use chrono::offset::TimeZone;
     use chrono::Utc;
+    use futures_util::stream::StreamExt;
 
-    #[test]
-    fn get_history() {
+    #[tokio::test]
+    async fn get_history() {
         let capacity = 2;
 
         let handler = RaidHandler::new(capacity);
-
         let boss_name_ja = BossName::from("Lv60 オオゾラッコ");
+        let boss_name_en = BossName::from("Lvl 60 Ozorotter");
+
+        let mut subscriber_ja = handler.subscribe(boss_name_ja.clone());
+        let mut subscriber_en = handler.subscribe(boss_name_en.clone());
+
         let raid1 = Raid {
             id: "1".into(),
             tweet_id: 1,
@@ -197,6 +263,7 @@ mod test {
         };
 
         assert!(handler.get_history(boss_name_ja.clone()).is_empty());
+        assert!(handler.get_history(boss_name_en.clone()).is_empty());
         assert!(handler.bosses().is_empty());
 
         handler.push(raid1.clone());
@@ -205,11 +272,11 @@ mod test {
             vec![Arc::new(raid1.clone())]
         );
         assert_eq!(handler.bosses(), vec![Arc::new(Boss::from(&raid1))]);
+        assert_eq!(subscriber_ja.next().await.unwrap(), Arc::new(raid1.clone()));
 
         let mut raid2 = raid1.clone();
         raid2.id = "2".into();
         raid2.tweet_id = 2;
-        raid2.user_name = "walfie_game".into();
         raid2.created_at = raid1.created_at + chrono::Duration::minutes(1);
         raid2.text = None;
 
@@ -219,6 +286,7 @@ mod test {
             handler.get_history(boss_name_ja.clone()),
             vec![Arc::new(raid2.clone()), Arc::new(raid1.clone())]
         );
+        assert_eq!(subscriber_ja.next().await.unwrap(), Arc::new(raid2.clone()));
 
         // When capacity is full, old entries should be overwritten
         let mut raid3 = raid2.clone();
@@ -230,9 +298,9 @@ mod test {
             handler.get_history(boss_name_ja.clone()),
             vec![Arc::new(raid3.clone()), Arc::new(raid2.clone())]
         );
+        assert_eq!(subscriber_ja.next().await.unwrap(), Arc::new(raid3.clone()));
 
         // Push a raid from a boss with a different name
-        let boss_name_en = BossName::from("Lvl 60 Ozorotter");
         let mut raid4 = raid3.clone();
         raid4.id = "4".into();
         raid4.tweet_id = 4;
@@ -244,6 +312,7 @@ mod test {
             handler.get_history(boss_name_en.clone()),
             vec![Arc::new(raid4.clone())]
         );
+        assert_eq!(subscriber_en.next().await.unwrap(), Arc::new(raid4.clone()));
 
         // Merge the two bosses. The history should be merged, as well as the boss entries and broadcast.
         handler.merge(boss_name_en.clone(), boss_name_ja.clone());
@@ -267,7 +336,38 @@ mod test {
             },
             ..Boss::from(&raid1)
         });
-        assert_eq!(handler.boss(boss_name_en), Some(expected_boss.clone()));
-        assert_eq!(handler.boss(boss_name_ja), Some(expected_boss));
+        assert_eq!(
+            handler.boss(boss_name_en.clone()),
+            Some(expected_boss.clone())
+        );
+        assert_eq!(handler.boss(boss_name_ja.clone()), Some(expected_boss));
+
+        // The next raid should get sent to both `en` and `ja` subscribers
+        let mut raid5 = raid4.clone();
+        raid5.id = "5".into();
+        raid5.tweet_id = 5;
+        raid5.boss_name = boss_name_ja.clone();
+        raid5.created_at = raid4.created_at + chrono::Duration::minutes(1);
+        {
+            let raid5 = raid5.clone();
+            let handler = handler.clone();
+            tokio::spawn(async move {
+                // Arbitrarily chosen delay to make the test work.
+                // Not sure of a good workaround here.
+                tokio::time::delay_for(std::time::Duration::from_millis(500)).await;
+                handler.push(raid5);
+            });
+        }
+        assert_eq!(subscriber_en.next().await.unwrap(), Arc::new(raid5.clone()));
+        assert_eq!(subscriber_ja.next().await.unwrap(), Arc::new(raid5.clone()));
+
+        let mut raid6 = raid5.clone();
+        raid6.id = "6".into();
+        raid6.tweet_id = 6;
+        raid6.boss_name = boss_name_en.clone();
+        raid6.created_at = raid5.created_at + chrono::Duration::minutes(1);
+        handler.push(raid6.clone());
+        assert_eq!(subscriber_en.next().await.unwrap(), Arc::new(raid6.clone()));
+        assert_eq!(subscriber_ja.next().await.unwrap(), Arc::new(raid6.clone()));
     }
 }
